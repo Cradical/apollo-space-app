36d994172f11acfb1dedaa4aae0a61a798bd9b01
Conrad Wright <easp.conrad@gmail.com>
2019-09-25 11:34:40 -0600

    Add get methods for launch data

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,57 +1,56 @@
 const { gql } = require('apollo-server')
 
-const typeDefs = gql``
+const typeDefs = gql`
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    tripe: [Launch]!
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Mutation {
+    #if false, booking failed -- check errors
+    bookTrips(LaunchIds: [ID]!): TripUpdateResponse!
+    #if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+`
 
 module.exports = typeDefs
-
-type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
-  me: User
-}
-
-type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
-}
-
-type Mission {
-  name: String
-  missionPatch(size: PatchSize): String
-}
-
-type Rocket {
-  id: ID!
-  name: String
-  type: String
-}
-
-type User {
-  id: ID!
-  email: String!
-  tripe: [Launch]!
-}
-
-enum PatchSize {
-  SMALL
-  LARGE
-}
-
-// Mutations
-
-type Mutation {
-  bookTrips(LaunchIds: [ID]!): TripUpdateResponse!
-
-  cancelTrip(launchId: ID!): TripUpdateResponse!
-
-  login(email: String): String
-}
-
-type TripUpdateResponse {
-  success: Boolean!
-  message: String
-  launches: [Launch]
-}
