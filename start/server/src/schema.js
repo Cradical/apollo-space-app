c97d1920701008a0322458387b407385cfdfd46b
Conrad Wright <easp.conrad@gmail.com>
2019-09-25 10:01:48 -0600

    Initialize db schema

---
+++ start/server/src/schema.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql``
+
+module.exports = typeDefs
+
+type Query {
+  launches: [Launch]!
+  launch(id: ID!): Launch
+  me: User
+}
+
+type Launch {
+  id: ID!
+  site: String
+  mission: Mission
+  rocket: Rocket
+  isBooked: Boolean!
+}
+
+type Mission {
+  name: String
+  missionPatch(size: PatchSize): String
+}
+
+type Rocket {
+  id: ID!
+  name: String
+  type: String
+}
+
+type User {
+  id: ID!
+  email: String!
+  tripe: [Launch]!
+}
+
+enum PatchSize {
+  SMALL
+  LARGE
+}
